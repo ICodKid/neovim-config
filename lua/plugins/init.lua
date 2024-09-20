efd9e9c2e3f401d8a9f211d5ccc00394237cb7f3
Oresti Tushe <orestitushe@Orestis-MacBook-Pro.local>
2024-09-20 12:17:45 +0100

    Merge

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,6 +16,10 @@ return require('packer').startup(function(use)
         {run = ':TSUpdate'}
     }
     use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    use {
         'nvim-treesitter/playground'
     }
     use {
@@ -26,10 +30,6 @@ return require('packer').startup(function(use)
     }
     use {
         'tpope/vim-fugitive'
-    }
-    -- Better file manager
-    use {
-        'nvim-tree/nvim-tree.lua'
     }
 	use {
   		'nvim-telescope/telescope.nvim', tag = '0.1.5',
@@ -56,6 +56,41 @@ return require('packer').startup(function(use)
 	use {
 		'mbbill/undotree'
 	}
+	use {
+		'tpope/vim-fugitive'
+	}
+
+    -- Tmux Naviagation
+    use {
+        'christoomey/vim-tmux-navigator'
+    }
+
+    -- LSP Zero
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment the two plugins below if you want to manage the language servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
+
+    -- Github Copilot When on new PC use the `:copilot setup` command 
+    use {
+        'github/copilot.vim'
+    }
+<<<<<<< HEAD
+
+=======
+>>>>>>> 0f3f7a7a17716198d315f22f665b87cc620686a0
     use { "vimsence/vimsence" }
     -- These optional plugins should be loaded directly because of a bug in Packer lazy loading
     use {'nvim-tree/nvim-web-devicons'} -- OPTIONAL: for file icons
